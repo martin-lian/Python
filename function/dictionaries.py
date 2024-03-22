cc3ac42754fc57605e9ad7bf9902dc1852804849
martin-lian <martin.agassi@gmail.com>
2024-03-22 16:58:01 +0530

    dict and list comp

---
+++ function/dictionaries.py
@@ -0,0 +1,26 @@
+#unordered, CHANGEABLE and Not allow dupilicates
+# Dictionaries Symbol - {}
+dict = {
+    #"KEY":"VALUES"
+    "Name":"Maritn J",
+    "Age":29,
+    "DOB":"19-01-1995",
+    "Fruits":["Watermelon","Mango","Grapes"],
+    "Vehicle":"Skoda"
+}
+print(dict)
+
+    #VIEW
+# x=dict["Fruits"]
+# print(x)
+    #OR
+# x=dict.get("Fruits")
+# print(x)
+
+    #MODIFY
+# dict["Vehicle"]="Skoda Rapid"
+# print(dict)
+
+    #POP
+# dict.pop("Fruits")
+# print(dict)
---
+++ function/listcomp.py
@@ -0,0 +1,11 @@
+fruits=["apple",'mango','kiwi','orange']
+"""
+exname=[]
+for x in fruits:
+    if "a" in x:
+        exname.append(x)
+print(exname)          
+"""
+
+xname=[x.upper() for x in fruits]
+print(xname)
